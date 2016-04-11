bd057de4d6d85da57e85873b46551623b5378f8d
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-11 20:35:01 +0200

    Empty task published

---
+++ D_CS_mondayCollections/MondayCollections.cs
@@ -0,0 +1,17 @@
+﻿/*
+Create a program that organizes the strings given by the user! Use the ArrayList collection!
+Create a program that shows the integers - given by the user - in reverse sequence! Use Stack to change the sequence!
+Create a program that changes the written numbers in a text to numerals ( e.g.: “three” - 3) The string and the numerical pairs should be stored in a Dictionary type!
+Create your own generic storage to solve the problem described in Task 1.!
+Create a generic LinkedList, wherein the elements contains three different types of value (the generic type has three type parameter)!
+*/
+
+namespace D_CS_mondayCollections {
+
+	class MondayCollections {
+
+		static void Main (string[] args) {
+
+		}
+	}
+}
