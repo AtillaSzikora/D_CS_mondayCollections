c1a4096621d7cece0560e181b16d6040debe0db6
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-11 22:59:30 +0200

    Shits not workin. :|

--- D_CS_mondayCollections/NumberSwapper.cs
+++ D_CS_mondayCollections/NumberSwapper.cs
@@ -1,10 +1,5 @@
-﻿/*
-Create a program that organizes the strings given by the user! Use the ArrayList collection!
-Create a program that shows the integers - given by the user - in reverse sequence! Use Stack to change the sequence!
-Create a program that changes the written numbers in a text to numerals ( e.g.: “three” - 3) The string and the numerical pairs should be stored in a Dictionary type!
-Create your own generic storage to solve the problem described in Task 1.!
-Create a generic LinkedList, wherein the elements contains three different types of value (the generic type has three type parameter)!
-*/
+﻿// Create a program that changes the written numbers in a text to numerals ( e.g.: “three” - 3)
+// The string and the numerical pairs should be stored in a Dictionary type!
 
 using System;
 using System.Collections.Generic;
@@ -12,18 +7,16 @@ using System.Collections.Generic;
 namespace NumberSwapper {
 	internal class NumberSwapper {
 
-		private static readonly char[] Delimiters = {' ', ',', '.', ':', ';', '!', '?', '/', '-'};
-		private readonly Dictionary<int, string> _numPairs = new Dictionary<int, string> {
-			{0, "zero"}, {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"},
-			{5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"} };
+		private static readonly Dictionary<string, string> NumPairs = new Dictionary<string, string> {
+			{"0", "zero"}, {"1", "one"}, {"2", "two"}, {"3", "three"}, {"4", "four"},
+			{"5", "five"}, {"6", "six"}, {"7", "seven"}, {"8", "eight"}, {"9", "nine"} };
 
 		private static string SwapNumsToNames(string textToChange) {
-			for (var i = 0; i < textToChange.Length - 3; i++) {
-				if (textToChange.Substring(i, 4).Equals("zero")) Console.WriteLine("Won."); }
-			return "The End."; }
+			string changedText = "";
+			foreach (var numPair in NumPairs) { changedText = textToChange.Replace(numPair.Value, numPair.Key); }
+			return changedText; }
 
 		private static void Main(string[] args) {
-			SwapNumsToNames("zero,one two.three-zero:four;five/zero?six");
-		}
+			Console.WriteLine(SwapNumsToNames("zero,one two.three-zero:four;five/zero?six")); }
 	}
 }
